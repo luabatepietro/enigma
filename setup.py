da7bda410008c066407c1c1e5c3233ec005c73a6
luabatepietro <luabatepietroapple@icloud.com>
2024-09-03 19:02:09 -0300

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="codificacao_algebra_linear",  # Nome do pacote
+    name="enigma",  # Nome do pacote
     version="0.1.0",  # Versão do pacote
     packages=find_packages(),  # Encontrar automaticamente todos os pacotes
     install_requires=[  # Dependências
@@ -15,10 +15,10 @@ setup(
     },
     author="Lucas Abatepietro",  # Seu nome
     author_email="luabatepietro@hotmail.com",  # Seu email
-    description="Sistema de Codificação e Decodificação utilizando Álgebra Linear",
+    description="Enigma",
     long_description=open("README.md").read(),  # Descrição longa (usualmente do README)
     long_description_content_type="text/markdown",
-    url="https://github.com/luabatepietro/codificacao_algebra_linear.git",  # URL do seu repositório
+    url="https://github.com/luabatepietro/enigma.git",  # URL do seu repositório
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
